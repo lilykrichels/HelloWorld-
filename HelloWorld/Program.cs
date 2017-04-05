9277d197f7858eb22cefeac054face6b17392d18
lilykrichels <lily.krichels@gmail.com>
2017-04-05 16:18:08 -0400

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("What is Your Name?");
+            //string donorname = Console.ReadLine();
+            //Console.WriteLine(donorname);
+            //Console.WriteLine("What is Your Address?");
+            //string donoraddress = Console.ReadLine();
+            //Console.WriteLine("What is your email?");
+            //string donoremail = Console.ReadLine();
+            //Console.WriteLine("What is the Fiscal Year?");
+            //string fiscalyear = Console.ReadLine();
+            //Console.WriteLine("How Many $100's do you have?");
+            //int hunds = int.Parse(Console.ReadLine());
+            //int hundsTotal = hunds * 100;
+            //Console.WriteLine("How Many $50's do you have?");
+            //int fifties = int.Parse(Console.ReadLine());
+            //int fiftotal = fifties * 50;
+            //Console.WriteLine("How Many $20's do you have?");
+            //int twents = int.Parse(Console.ReadLine());
+            //int twentotal = twents * 20;
+            //Console.WriteLine("How Many $10's do you have?");
+            //int tens = int.Parse(Console.ReadLine());
+            //int tenstotal = tens * 10;
+            //Console.WriteLine("How many $5's do you have?");
+            //int five = int.Parse(Console.ReadLine());
+            //int fivetotal = five * 5;
+            //Console.WriteLine("How many ones do you have?");
+            //int one = int.Parse(Console.ReadLine());
+            //int onetotal = one * 1;
+            //Console.WriteLine("How many quarters do you have?");
+            //int quarters = int.Parse(Console.ReadLine());
+            //double quarterstotal = quarters * .25d;
+            //Console.WriteLine(hundsTotal + fiftotal + twentotal + tenstotal + fivetotal + onetotal + quarterstotal);
+
+            
+
+
+            //int B = int.Parse(Console.ReadLine());
+            //int Btotal = B * 3;
+            //Console.WriteLine("How many C's do you have?");
+        
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
